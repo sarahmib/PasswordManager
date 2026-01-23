0f7393cc8007bcf67c48a501bab061750aa9845e
sarahmib <sarahmibeecroft@gmail.com>
2026-01-22 20:42:43 -0700

    NUKE IT SIRE

--- Models/UserInformation.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace PasswordManager.Models;
-
-public class UserInformation
-{
-    public string username;
-    public string password;
-}
--- ViewModels/BaseViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.ComponentModel;
-
-namespace PasswordManager.Utility;
-
-public class BaseViewModel : INotifyPropertyChanged
-{
-    
-}
